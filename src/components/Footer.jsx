c7e9b791cc01e21f0e2a9710410fe23ec4c0fa6e
Kevin Alirio Almario Agudelo <k.almario@udla.edu.co>
2026-05-06 13:08:27 -0500

    Delete src directory

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="footer-bar">
-      <p>Administración de joyería — Kevin Almario, Mauricio Lopez </p>
-    </footer>
-  )
-}
-
-export default Footer
